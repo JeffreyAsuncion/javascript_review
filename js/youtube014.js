8c17a395bdeb608ac2c94b0ac081e0bf795c0332
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-26 15:39:26 -0400

    operators and pemdas

---
+++ js/youtube014.js
@@ -0,0 +1,22 @@
+{
+    var slicesOfPizza = 10;
+    console.log(slicesOfPizza + 10);
+    console.log(slicesOfPizza - 10);
+    console.log(slicesOfPizza * 10);
+    console.log(slicesOfPizza / 10);
+    console.log(slicesOfPizza % 10);
+
+    var precedence1 = 5 + 3 * 12 - 20 / 10;
+    var precedence2 = 5 + (3 * 12) - (20 / 10);
+    var precedence3 = (5 + 3) * (12 - 20) / 10;
+    console.log(precedence1, precedence2, precedence3);
+    /*
+
+    +
+    -
+    *
+    /
+    %
+
+    */
+}
