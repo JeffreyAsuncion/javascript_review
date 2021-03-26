2d93652cc5fdd5826b6452a4f6fd8c92f7413dae
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-26 15:47:52 -0400

    increment

---
+++ js/youtube015.js
@@ -0,0 +1,9 @@
+{
+    let slicesOfPizza = 10;
+    slicesOfPizza = slicesOfPizza + 1;
+    console.log(slicesOfPizza);
+    slicesOfPizza++;
+    console.log(slicesOfPizza);
+    slicesOfPizza *= 5;
+    console.log(slicesOfPizza);
+}
