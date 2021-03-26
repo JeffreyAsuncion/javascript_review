cb6170a7caafa978d70261999a4dbbc2fcda48fa
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-26 15:03:45 -0400

    primitives and objects

---
+++ js/youtube.js
@@ -0,0 +1,35 @@
+{
+    let x = 5; 
+    let b = "10";
+
+    x = x + b;
+    console.log(x);
+
+    let myName = 'Caleb';
+    let tacos; // undefined
+}
+
+// primitives
+// objects
+
+// Time Stamp 1:10:32
+
+
+// create an object made with Key-value pairs
+
+let person = {
+    name : "Caleb", // properties or members or field(privatge properites)
+    age: 93,
+    favFood: "Pizza... Obviously -_-",
+
+    // method
+    fun: function(){
+        console.log("Yay!");
+    }
+}
+
+person.fun();
+
+let now = new Date();
+
+let grades = [30, 30, 12, 23];
