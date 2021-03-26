55f254fe5cf8a446d4d6fbc5f9a5df87fffde7d6
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-26 15:09:49 -0400

    more objects

---
+++ js/youtube012.js
@@ -0,0 +1,8 @@
+{
+    let myName = "Caleb";
+    let yourName = new String("Claire")
+    myName = myName.toUpperCase();
+    console.log(myName);
+    console.log(typeof(myName));
+    console.log(typeof(yourName));
+}
