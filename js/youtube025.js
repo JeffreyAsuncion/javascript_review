a8b60f849b2da96aae26a8cddc0d76cb2aeab1b8
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-27 11:31:02 -0400

    comparison operators;

---
+++ js/youtube025.js
@@ -0,0 +1,16 @@
+{
+    var name = prompt();
+    if(name === "Caleb"){
+        console.log("Welcome!");
+    }
+
+    var name2 = prompt();
+    if(name2 !== "Claire"){
+        console.log("Welcome! Not Claire");
+    }
+
+    var age = prompt();
+    if(age >= 12){
+        console.log("Welcome! you are 12 or older");
+    }
+}
