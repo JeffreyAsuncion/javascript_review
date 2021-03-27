b753e16aa466ab075131bc8c999964b7b50959b8
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-26 23:36:34 -0400

    String methods

---
+++ js/youtube019.js
@@ -0,0 +1,14 @@
+{
+    let myName = "Caleb";
+    console.log(myName[2]);
+    console.log("My Name is " + myName + "!");
+    console.log(`My Name is ${myName}!`);
+    let yourName = new String("Claire");
+
+    let test = "lorem ipsum... This is a really great string full of lots of valuable\
+     information that everyone needs to know.";
+
+     console.log(test);
+     console.log(test.length);
+    
+}
