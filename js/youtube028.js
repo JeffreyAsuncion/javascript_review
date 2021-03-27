56a48cc4e15fd342fdfabf76021b759932c495f2
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-27 13:17:44 -0400

    single line if statement

---
+++ js/youtube028.js
@@ -0,0 +1,11 @@
+{
+    let name = prompt("What is your name?");
+    if(name === "Claire") console.log("Hello!");
+
+    if(name === "Mark") { console.log("Hello!"); }
+
+    if(name === "Jeffrey") { 
+        console.log("Hello!"); 
+    }
+
+}
