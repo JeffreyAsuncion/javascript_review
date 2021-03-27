f50b9cb9232daa8a77bd1c7a1c534d59a473bc0c
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-27 08:02:36 -0400

    generalization

---
+++ js/youtube023.js
@@ -0,0 +1,11 @@
+{
+    var name = prompt();
+
+    if(name === "Caleb"){   // identity operator // strict equality
+        console.log("Welcome!");
+    } else if(name ==="Sally"){
+        console.log("Hey Sally!");
+    } else { // else is a catch all
+        console.log("What up homeslice!");
+    }
+}
---
+++ js/youtube024.js
@@ -0,0 +1,16 @@
+{
+    var name = "Caleb"; // hard coding for testing
+    // var name = prompt();
+
+    if(name === "Caleb"){   // identity operator // strict equality
+        console.log("Welcome!");
+    } else if(name ==="Sally"){
+        console.log("Hey Sally!");
+    } else { // else is a catch all
+        console.log("What up homeslice!");
+    }
+}
+
+// generalization 
+// our program works in various situations
+
