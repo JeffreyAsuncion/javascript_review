6051e057c657fc5aa10dd78ef4ac0079cde008d3
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-27 21:02:26 -0400

    Nested Loops

---
+++ js/youtube034.js
@@ -0,0 +1,10 @@
+{
+    let d = document.getElementById("destination");
+    for(let i = 0; i < 10; i++){
+        for (let k = i; k >= 0; k--){
+            d.append(k + " ");
+        }
+        let br = document.createElement('br');
+        d.appendChild(br);
+    }
+}
