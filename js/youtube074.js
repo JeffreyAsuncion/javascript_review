84911c2b62440b8e28112b12a1b24f1177661cc2
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-03-30 12:14:37 -0400

    prototype functions

--- js/youtube074.js
+++ js/youtube074.js
@@ -2,14 +2,13 @@
     function User(name, interests){
         this.name = name;
         this.interests = interests;
-        this.outputStuff = function(){
-            console.log("My name  " + this.name, this.interests);
-        }
+        // this.outputStuff = function(){
+        //     console.log("My name  " + this.name, this.interests);
+        // }
     }
     User.prototype.greet = function(){
         console.log("My name is " + this.name, this.interests);
     }
-
 
     let me = new User("Caleb", ["cooking", "eating", "exercise"]);
     let you = new User("Camila", ["hotwheels"]);
